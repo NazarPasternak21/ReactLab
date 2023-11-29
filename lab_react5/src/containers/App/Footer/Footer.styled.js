41cff106ca426811042b71fb977f8fd6969aab0e
NazarPasternak21 <119297986+NazarPasternak21@users.noreply.github.com>
2023-11-29 22:19:29 +0200

    Add files via upload

---
+++ lab_react5/src/containers/App/Footer/Footer.styled.js
@@ -0,0 +1,14 @@
+const styles = {
+    footer: {
+        marginTop: "50px",
+    },
+    logo: {
+        margin: "0 auto",
+        width: "50px"
+    },
+    verticalAlignCenter: {
+        marginTop: "20px"
+    },
+}
+
+export default styles;
---
+++ lab_react5/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './containers/App/App';
+import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Catalog from './containers/Catalog/Catalog';
+import ItemPage from './containers/ItemPage/ItemPage'
+import CardPage from './containers/CartPage/CartPage';
+import { Provider } from 'react-redux';
+import store from './containers/Redux/store';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, 
+  },
+  {
+    path: '/catalog',
+    element: <Catalog />, 
+  },
+  {
+    path: '/catalog/:id',
+    element: <ItemPage/>
+  },
+  {
+    path: '/cart',
+    element: <CardPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}>
+        <Route path="/" element={<App />} />
+      </RouterProvider>
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
